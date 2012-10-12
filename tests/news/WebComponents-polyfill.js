4740b6431c84908ad582f9ee2052708a20a44d21
Sander Spies <sandermail@gmail.com>
2012-10-12 10:45:51 +0200

    Initial support for constructor + scripting tag
    Does not support lifecycle, and is very gross in extending the
    constructor tag. Perhaps need to do some parsing later on to properly
    support scripting.

--- tests/news/WebComponents-polyfill.js
+++ tests/news/WebComponents-polyfill.js
@@ -14,39 +14,70 @@
 
 				
 				var doc = !window.ActiveXObject ? document.implementation.createHTMLDocument('', 'html', null) : new ActiveXObject("htmlfile");
-////var a = ; a.open(); a.write("<html><head><link /></head><body>22222<body></html>"); a.close(); console.log(a.documentElement.innerHTML)
-				console.log(doc);
 				doc.open("text/html", "replace");
 				
 				doc.write(xhr.responseText);
-				console.log(xhr.responseText);
-
-				//doc.write(xhr.responseText);
-				//doc.close();
-
+				doc.close();
 				var elements = doc.querySelectorAll("element");
 				for(var i = 0, l = elements.length; i < l; i++){
 					var element = elements[i];
 					var name = element.getAttribute("name");
 					webComponents[name] = {};
 					webComponents[name].document = doc;
-					webComponents[name].element = element;	
+					webComponents[name].element = element;
+					webComponents[name].extends = element.getAttribute("extends");
+					webComponents[name].constructor = element.getAttribute("constructor");
 				}
 			}
 		}
 		
+
+
+		//TODO: need a better way that also supports createElementNS...
+		document.createElement = function createElement(txt){
+			if(webComponents[txt]){
+				var component = document.createElementNS("http://www.w3.org/1999/xhtml", webComponents[txt].extends);
+				component.setAttribute("is", txt);
+				return component;
+			}
+			else {
+				return document.createElementNS("http://www.w3.org/1999/xhtml", txt);
+			}
+		};
+
+		// make sure we can invoke the elements in JS
+		// need to do some extra work here...
 		for(var compName in webComponents){
-			console.log(compName, webComponents[compName]);
+			var component = webComponents[compName];
+			if(component.constructor){
+				var proto = "";
+				if(webComponents[compName].element.getElementsByTagName("script").length > 0){
+					for(var i = 0, l = webComponents[compName].element.getElementsByTagName("script").length; i < l; i++){
+						proto += webComponents[compName].element.getElementsByTagName("script")[i].innerHTML;
+					}
+				}
+
+				//todo (function(){ this.lifecycle })(parentEl)
+				eval("window['" + component.constructor + "'] = function " + component.constructor + "(el){if(!el){el = document.createElement('" + compName + "');} " +
+					proto.replace(new RegExp(component.constructor + ".prototype", "g"), "el") + " return el;} " );
+				//TODO: add constructor prototype support via script tag
+
+			}
+		}
+		setTimeout(function(){
+		for(var compName in webComponents){
 			var usingEls = document.querySelectorAll("[is='" + compName + "']");
 			for(var i = 0, l = usingEls.length; i < l; i++){
 				var el = usingEls[i];
-				var shadowDom = new ShadowRootPolyfill(el);
-//				console.log(webComponents[compName].element.getElementsByTagName("template")[0]);
-				shadowDom.innerHTML(webComponents[compName].element.getElementsByTagName("template")[0].innerHTML);
+				if(webComponents[compName].element.getElementsByTagName("template").length > 0){
+					var shadowDom = new ShadowRootPolyfill(new window[component.constructor](el));
+					shadowDom.innerHTML(webComponents[compName].element.getElementsByTagName("template")[0].innerHTML);
+				}
 			}
 		}
+		}, 0);
 
-		console.log("web components:", webComponents);
+			//TODO: listen to dom manipulation ('is=' stuff)
 
 		//TODO: add web components one by one
 		// outer el is determined by extends=""
