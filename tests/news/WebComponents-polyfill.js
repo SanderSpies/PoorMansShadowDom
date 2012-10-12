ef165f21063afe17e8160707f04d4b1672abd467
Sander Spies <sandermail@gmail.com>
2012-10-12 17:50:48 +0200

    Starting work on wc lifecycle + some extra tests
    - create and inserted have been implemented
    - not sure about having the correct this value in the wc script tag yet
    - now also possible to use appendChild on element later on
    - several evil dom extensions (createElement, appendChild) of which
    probably more will follow
    - pondering how extending wc's will happen

--- tests/news/WebComponents-polyfill.js
+++ tests/news/WebComponents-polyfill.js
@@ -1,25 +1,25 @@
-(function(){
+(function () {
 	var webComponents = {};
 
-	var WebComponentsLoader = function WebComponentsLoader(){
+	var WebComponentsLoader = function WebComponentsLoader() {
 		var linkTags = document.querySelectorAll("link");
-		for(var i = 0, l = linkTags.length; i < l; i++){
+		for (var i = 0, l = linkTags.length; i < l; i++) {
 			var linkTag = linkTags[i];
 			var relAttribute = linkTag.getAttribute("rel");
-			if(relAttribute && relAttribute === "components"){
+			if (relAttribute && relAttribute === "components") {
 				var href = linkTag.getAttribute("href");
 				var xhr = new XMLHttpRequest();
 				xhr.open("get", href, false);
 				xhr.send(null);
 
-				
+
 				var doc = !window.ActiveXObject ? document.implementation.createHTMLDocument('', 'html', null) : new ActiveXObject("htmlfile");
 				doc.open("text/html", "replace");
-				
+
 				doc.write(xhr.responseText);
 				doc.close();
 				var elements = doc.querySelectorAll("element");
-				for(var i = 0, l = elements.length; i < l; i++){
+				for (var i = 0, l = elements.length; i < l; i++) {
 					var element = elements[i];
 					var name = element.getAttribute("name");
 					webComponents[name] = {};
@@ -30,59 +30,124 @@
 				}
 			}
 		}
-		
 
+		/* start of evil dom overrides */
+		var proto2 = HTMLElement.prototype;
+		var originalAppendChild = proto2.appendChild;
+		proto2.appendChild = function HTMLElement$appendChild(child) {
+			originalAppendChild.call(this, child);
+			var isAttribute = child.getAttribute("is")
 
-		//TODO: need a better way that also supports createElementNS...
-		document.createElement = function createElement(txt){
-			if(webComponents[txt]){
-				var component = document.createElementNS("http://www.w3.org/1999/xhtml", webComponents[txt].extends);
-				component.setAttribute("is", txt);
-				return component;
+			if (isAttribute) {
+				if (!(this.children[0] instanceof HTMLIFrameElement)) {
+					for (var compName in webComponents) {
+						if (compName === isAttribute) {
+							var component = webComponents[compName];
+							var shadowDom = new ShadowRootPolyfill(new window[component.constructor](child));
+							shadowDom.innerHTML(component.element.getElementsByTagName("template")[0].innerHTML);
+						}
+					}
+				}
 			}
-			else {
-				return document.createElementNS("http://www.w3.org/1999/xhtml", txt);
+			else if (!(child instanceof HTMLIFrameElement) && (this.children[0] instanceof HTMLIFrameElement) ) {
+				var detached = child.parentNode.removeChild(child);
+				var containerDocument = this.children[0].contentDocument;
+				var insertionPoints = containerDocument.querySelectorAll("span[class='insertionPointEnd']");
+				var nullSelector = null;
+				var temp = document.createElement("div");
+				temp.appendChild(detached);
+				for(var i = 0, l = insertionPoints.length; i < l; i++){
+					var insertionPoint = insertionPoints[i];
+					var selector = insertionPoint.getAttribute("select");
+					if(selector === null){
+						nullSelector = insertionPoint;
+					}
+					else {
+						var els = temp.querySelectorAll(selector);
+						for(var j = 0, len = els.length; j < len; j++){
+							var detached2 = temp.removeChild(els[j]);
+							insertionPoint.parentNode.insertBefore(detached2, insertionPoint);
+						}
+					}
+				}
+
+				if(nullSelector){
+					for(var i = 0, l = temp.children.length; i < l; i++){
+						nullSelector.parentNode.insertBefore(temp.children[i], nullSelector);
+					}
+				}
+
+				if (containerDocument.body.scrollHeight > this.offsetHeight) {
+					this.style.height = containerDocument.body.scrollHeight + "px";
+					this.children[0].style.height = containerDocument.body.scrollHeight + "px";
+				}
 			}
 		};
 
-		// make sure we can invoke the elements in JS
-		// need to do some extra work here...
-		for(var compName in webComponents){
+		var originalCreateElement = document.createElement;
+		document.createElement = function createElement(txt, skip) {
+			if (webComponents[txt]) {
+				var component = new (window[webComponents[txt].constructor])();
+				return component;
+			}
+			else {
+				return originalCreateElement.call(document, txt);
+			}
+		};
+
+		window["HTMLElementElement"] = function HTMLElementElement(){
+
+		};
+		window["HTMLElementElement"].prototype = HTMLElement.prototype;
+
+		window["HTMLElementElement"].prototype.lifecycle = function(obj){
+			this._lifecycle = obj;
+		};
+
+		/* end of evil dom overrides*/
+
+		/* start of javascript support */
+		for (var compName in webComponents) {
 			var component = webComponents[compName];
-			if(component.constructor){
+			component.elementInstance = new HTMLElementElement();
+
+			if (component.constructor) {
 				var proto = "";
-				if(webComponents[compName].element.getElementsByTagName("script").length > 0){
-					for(var i = 0, l = webComponents[compName].element.getElementsByTagName("script").length; i < l; i++){
+				if (webComponents[compName].element.getElementsByTagName("script").length > 0) {
+					for (var i = 0, l = webComponents[compName].element.getElementsByTagName("script").length; i < l; i++) {
 						proto += webComponents[compName].element.getElementsByTagName("script")[i].innerHTML;
 					}
 				}
 
-				//todo (function(){ this.lifecycle })(parentEl)
-				eval("window['" + component.constructor + "'] = function " + component.constructor + "(el){if(!el){el = document.createElement('" + compName + "');} " +
-					proto.replace(new RegExp(component.constructor + ".prototype", "g"), "el") + " return el;} " );
-				//TODO: add constructor prototype support via script tag
-
+				proto = "(function(){ var " + component.constructor + " = function " + component.constructor + "(){};  " + proto + "; if( " + component.constructor + "){ return " + component.constructor + ".prototype; } else { return null; } }).call(component.elementInstance)";
+				eval("window['" + component.constructor + "'] = function " + component.constructor + "(el){if(!el){el = originalCreateElement.call(document, '" + webComponents[compName].extends + "');} " +
+					"el.setAttribute('is', '" + compName + "'); var temp = " + proto + "; if(temp){for(var protoName in temp){el[protoName] = temp[protoName]; }} return el;} ");
 			}
 		}
-		setTimeout(function(){
-		for(var compName in webComponents){
+
+		/* end of javascript support */
+		for (var compName in webComponents) {
 			var usingEls = document.querySelectorAll("[is='" + compName + "']");
-			for(var i = 0, l = usingEls.length; i < l; i++){
+			for (var i = 0, l = usingEls.length; i < l; i++) {
 				var el = usingEls[i];
-				if(webComponents[compName].element.getElementsByTagName("template").length > 0){
-					var shadowDom = new ShadowRootPolyfill(new window[component.constructor](el));
+				if (webComponents[compName].element.getElementsByTagName("template").length > 0) {
+					var instance = new window[component.constructor](el);
+					var shadowDom = new ShadowRootPolyfill(instance);
+					if(webComponents[compName].elementInstance._lifecycle && webComponents[compName].elementInstance._lifecycle.created){
+						webComponents[compName].elementInstance._lifecycle.created.call(instance, shadowDom);
+					}
 					shadowDom.innerHTML(webComponents[compName].element.getElementsByTagName("template")[0].innerHTML);
+					if(webComponents[compName].elementInstance._lifecycle && webComponents[compName].elementInstance._lifecycle.inserted){
+						webComponents[compName].elementInstance._lifecycle.inserted.call(instance);
+					}
 				}
 			}
 		}
-		}, 0);
-
-			//TODO: listen to dom manipulation ('is=' stuff)
-
+		//
 		//TODO: add web components one by one
 		// outer el is determined by extends=""
 		// inner workings = shadow dom stuff -> use wc template and apply dom contents to it
-		
+
 		// - make sure extends works
 		// - keep using the css + js I guess? maybe adjust the path?!
 		// - handle the templating stuff ->
@@ -92,7 +157,7 @@
 		// - eventing stuff
 	};
 
-	
+
 	WebComponentsLoader.prototype = {
 
 	};
